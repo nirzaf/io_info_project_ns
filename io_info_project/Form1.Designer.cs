0a3dd890df7e060d3388288ed41de52e99a1dd82
nirzaf <mfmfazrin1986@gmail.com>
2022-02-07 20:44:01 +0530

    Add project files.

---
+++ io_info_project/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace io_info_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.id_textBox1 = new System.Windows.Forms.TextBox();
+            this.full_name_textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Full Name:";
+            // 
+            // id_textBox1
+            // 
+            this.id_textBox1.Location = new System.Drawing.Point(85, 28);
+            this.id_textBox1.Name = "id_textBox1";
+            this.id_textBox1.Size = new System.Drawing.Size(100, 20);
+            this.id_textBox1.TabIndex = 3;
+            // 
+            // full_name_textBox2
+            // 
+            this.full_name_textBox2.Location = new System.Drawing.Point(85, 63);
+            this.full_name_textBox2.Name = "full_name_textBox2";
+            this.full_name_textBox2.Size = new System.Drawing.Size(262, 20);
+            this.full_name_textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Load Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 313);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.full_name_textBox2);
+            this.Controls.Add(this.id_textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox id_textBox1;
+        private System.Windows.Forms.TextBox full_name_textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ io_info_project/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace io_info_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fn;
+            fn = "E:\\tuts\\info_" + this.id_textBox1.Text + ".txt";
+            if (this.id_textBox1.Text=="")
+            {
+                MessageBox.Show("Please enter your ID!");
+                return;
+            }
+            if (this.full_name_textBox2.Text == "")
+            {
+                MessageBox.Show("Please enter your Full Name!");
+                return;
+            }
+            System.IO.File.WriteAllText(fn, this.full_name_textBox2.Text, Encoding.UTF8);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fn;
+            fn = "E:\\tuts\\info_" + this.id_textBox1.Text + ".txt";
+            if (this.id_textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter your ID!");
+                return;
+            }
+            if (System.IO.File.Exists( fn )==false )
+            {
+                MessageBox.Show ("I can't find your info.Please try again!");
+                return;
+            }
+            //////////if (this.full_name_textBox2.Text == "")
+            //////////{
+            //////////    MessageBox.Show("Please enter your Full Name!");
+            //////////    return;
+            //////////}
+            // System.IO.File.WriteAllText(fn, this.full_name_textBox2.Text, Encoding.UTF8);
+            string r;
+            r = System.IO.File.ReadAllText(fn, Encoding.UTF8);
+            this.full_name_textBox2.Text = r;
+        }
+    }
+}
